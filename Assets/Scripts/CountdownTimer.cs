834a12b6ea919fab9b5d69e3f2ad192452ad0827
Namprik <wareesa1847@gmail.com>
2025-04-20 21:57:43 +0700

    feat: change format countdown timer to 00.00

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -21,6 +21,6 @@ public class CountdownTimer : MonoBehaviour
 
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText.text = string.Format("{0:00}.{1:00}", minutes, seconds);
     }
 }
